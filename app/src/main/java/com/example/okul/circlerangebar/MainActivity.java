6e6e6fac88fe9bd7ada5c1676be75a175f986ab3
irtengunica <irtengunica@gmail.com>
2019-12-14 17:49:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/okul/circlerangebar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.okul.circlerangebar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+private  int progressStatus=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
